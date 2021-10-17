167d1266303882d9ee4313163af48e4ea8e3d6b8
KAMI-KAZ-e <55592102+KAMI-KAZ-e@users.noreply.github.com>
2021-10-17 19:36:09 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,35 @@
 // var attempt = 3;
+const express = require("express");
+const app = express();
+
 function validate(){
     var username=document.getElementById("Username").value;
     var password=document.getElementById("Password").value;
+    // app.get('http://localhost:3000/account.html', (req, res) =>{
     if ( username == "admin" && password == "#123"){
         alert ("Login successful");
         window.open( "a.html"); // Redirecting to other page.
+        //res.status(200).send("/a.html");
+
+        // api url
+const api_url =
+"https://us-central1-virtual-assistance-1.cloudfunctions.net/app/a.html";
+
+// Defining async function
+async function getapi(url) {
+
+// Storing response
+const response = await fetch(url);
+
+// Storing data in form of JSON
+var data = await response.json();
+console.log(data);
+
+}
+// Calling that async function
+getapi(api_url);
+
+
 
         return false;
         }
@@ -13,6 +38,7 @@ function validate(){
             return false;
 
         }
-        }
+        // })
+}
         
         
